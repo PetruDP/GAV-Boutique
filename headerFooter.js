dae5c016fe43685ce501666eb350c1247363730d
Petru <prioteasa.petru@gmail.com>
2023-01-12 01:53:56 +0200

    Tablouri Canvas page

--- headerFooter.js
+++ headerFooter.js
@@ -1,11 +1,11 @@
 let currentPath = location.origin;
 let header = `
     <div>
-        <h1>GAV Boutique</h1>
+        <h1 class="font-effect-neon">GAV &nbspBoutique</h1>
         <div class = "barMenu">
             <a href = "${currentPath}/index.html" class = "homeLink">HOME</a>
             <a href = "${currentPath}/TablouriCanvas/tablouriCanvas.html" class = "tablouriLink">Tablouri canvas</a>
-            <a href = "${currentPath}/PasteCraciun/pasteCraciun.html" class = "pasteCraciunLink">Decoratiuni de Craciun</a>
+            <a href = "${currentPath}/PasteCraciun/pasteCraciun.html" class = "pasteCraciunLink"></a>
             <a href = "${currentPath}/AccesoriiBotez/accesoriiBotez.html" class = "accesoriiBotezLink">Accesorii botez</a>
             <a href = "#contact" class = "contactLink">CONTACT</a>
         </div>
@@ -18,18 +18,45 @@ document.querySelector(".contactLink").onclick = () => {
   document.querySelector(".contact").classList.add("contactAnimation");
   setTimeout(() => {
     document.querySelector(".contact").classList.remove("contactAnimation");
-  }, 500);
+  }, 1500);
 };
+
+let dot = "";
+
+if (location.origin != location.pathname) {
+  dot = ".";
+}
 
 let footer = `
     <div class = "contact" id = "contact">
-        <h1>CONTACT</h1>
-        <p><b>Comenzile se plaseaza la:</b></p>
-        <p><b>e-mail:</b> dinu.adrianavalentina@yahoo.ro</p>
-        <p><b>telefon:</b> 0764 614 058</p>
-        <p><b>whatsapp:</b> 0764 614 058</p>
-        <p><b>facebook messenger:</b> <a href = "#" >GAV-Boutique</a></p>
-        
+        <div>
+            <h1>CONTACT</h1>
+            <p style = "margin-top : 5px;"><b>Comenzile se plaseaza la:</b></p>
+        </div>
+        <div>    
+            <div>
+                <a href = "https://www.gmail.com" target = "_blank"> 
+                    <img src = "${dot}./img/gmail.svg">
+                </a>
+                <p><b>e-mail:</b> dinu.adrianavalentina@yahoo.ro</p>
+            </div> 
+            <div>
+                <img src = "${dot}./img/phone.svg">
+                <p><b>telefon:</b> 0764 614 058</p>
+            </div> 
+        </div>
+        <div>
+            <div>
+                <a href = "#">
+                    <img src = "${dot}./img/facebook2.svg">
+                </a>
+                <p><b>facebook messenger:</b> <a href = "#" >GAV-Boutique</a></p>
+            </div> 
+            <div>
+                <img src = "${dot}./img/whatsapp.svg"> 
+                <p><b>whatsapp:</b> 0764 614 058</p>
+            </div>                      
+        </div>
 
     </div>`;
 
--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+let bgPink = "rgb(233, 164, 167)";
+
 if (window.location.pathname == "/index.html") {
-  document.querySelector(".homeLink").style.backgroundColor =
-    "rgb(240, 226, 206)";
+  document.querySelector(".homeLink").style.backgroundColor = bgPink;
   document.querySelector(".homeLink").style.textDecoration = "underline";
 } else if (window.location.pathname == "/TablouriCanvas/tablouriCanvas.html") {
-  document.querySelector(".tablouriLink").style.backgroundColor =
-    "rgb(240, 226, 206)";
+  document.querySelector(".tablouriLink").style.backgroundColor = bgPink;
   document.querySelector(".tablouriLink").style.textDecoration = "underline";
 } else if (window.location.pathname == "/PasteCraciun/pasteCraciun.html") {
-  document.querySelector(".pasteCraciunLink").style.backgroundColor =
-    "rgb(240, 226, 206)";
+  document.querySelector(".pasteCraciunLink").style.backgroundColor = bgPink;
   document.querySelector(".pasteCraciunLink").style.textDecoration =
     "underline";
 } else if (window.location.pathname == "/AccesoriiBotez/accesoriiBotez.html") {
-  document.querySelector(".accesoriiBotezLink").style.backgroundColor =
-    "rgb(240, 226, 206)";
+  document.querySelector(".accesoriiBotezLink").style.backgroundColor = bgPink;
   document.querySelector(".accesoriiBotezLink").style.textDecoration =
     "underline";
+}
+
+let date = new Date();
+let currMonth = date.getMonth() + 1;
+
+if (currMonth >= 2 && currMonth <= 5) {
+  document.querySelector(".pasteCraciunLink").innerText =
+    "Decoratiuni de Paste";
+} else if (currMonth >= 6 && currMonth <= 9) {
+  document.querySelector(".pasteCraciunLink").style.display = "none";
+} else {
+  document.querySelector(".pasteCraciunLink").innerText =
+    "Decoratiuni de Craciun";
 }
